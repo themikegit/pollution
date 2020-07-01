5183a7481503ccbd7ec273bb514a50c21eecc432
Krsmanovic Miroslav - GrECo Shared Services Center <m.krsmanovic@greco.services>
2020-07-01 15:32:30 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ axios
+  .get(
+   `//https://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=66945069-266e-4ac7-8756-56f354653a92`
+  )
+  .then(res => {
+   const dataVal = res.data;
+   console.log("dataVal", dataVal);
+  });
+
+ return (
+  <div className="App">
+   <Card />
+  </div>
+ );
 }
 
 export default App;
+
+// Object
+// data:
+// city: "Los Angeles"
+// country: "USA"
+//current.pollution.aqius
+// pollution:
+// aqicn: 24
+// aqius: 62
+// maincn: "p2"
+// mainus: "p2"
+// ts: "2020-07-01T12:00:00.000Z"
+// __proto__: Object
+//weather:
+// hu: 68
+// ic: "04n"
+// pr: 1012
+// tp: 17
+// ts: "2020-07-01T12:00:00.000Z"
+// wd: 0
+// ws: 1.5
+// __proto__: Object
+// __proto__: Object
+// location: {type: "Point", coordinates: Array(2)}
+// state: "California"
+// __proto__: Object
+// status: "success"
+// __proto__: Object
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styling.css";
+export default function Card() {
+ return (
+  <div className="wrap">
+   <h2>hello</h2>
+  </div>
+ );
+}
