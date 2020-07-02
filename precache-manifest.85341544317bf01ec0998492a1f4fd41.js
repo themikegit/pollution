77ddf1bab0d9bf71c2b0164607ed7e99890eb305
Krsmanovic Miroslav - GrECo Shared Services Center <m.krsmanovic@greco.services>
2020-07-02 14:44:33 +0200

    Updates

---
+++ precache-manifest.85341544317bf01ec0998492a1f4fd41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4928d1bc82b97a5321c1199515b120",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078a3a126e8327cd251f",
+    "url": "/static/css/main.f255b5cb.chunk.css"
+  },
+  {
+    "revision": "8b7c12995739aaceb56e",
+    "url": "/static/js/2.7609f6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7609f6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078a3a126e8327cd251f",
+    "url": "/static/js/main.4cd3e50c.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
